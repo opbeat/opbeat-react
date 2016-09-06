20542df46f1a5274c2292c9c05cf8024ab4679ad
Ron Cohen <ron@opbeat.com>
2016-09-06 21:34:41 +0200

    Add test for REPLACE route

--- test/react/router.spec.js
+++ test/react/router.spec.js
@@ -1,4 +1,5 @@
 var makeSignatureFromRoutes = require('../../src/react/router').makeSignatureFromRoutes
+var pushLocation = {action: 'PUSH'}, replaceLocation = {action: 'REPLACE'}
 
 describe("react: makeSignatureFromRoutes", function() {
   it("should correctly join paths", function() {
@@ -7,16 +8,28 @@ describe("react: makeSignatureFromRoutes", function() {
       {path: "/"}, {path: "/something"}, 
     ]
 
-    expect(makeSignatureFromRoutes(routes)).toBe("/something")
+    var pushLocation = {
+      action: 'PUSH'
+    }
+
+    expect(makeSignatureFromRoutes(routes, pushLocation)).toBe("/something")
     
     routes = [
       {path: "/"}, {path: "something"}, 
     ]
 
-    expect(makeSignatureFromRoutes(routes)).toBe("/something")
+    expect(makeSignatureFromRoutes(routes, pushLocation)).toBe("/something")
   })
 
   it("should handle zero routes", function() {
-    expect(makeSignatureFromRoutes([])).toBe("unknown")
+    expect(makeSignatureFromRoutes([], pushLocation)).toBe("unknown")
+  })
+
+
+  it("should handle REPLACE routes", function() {
+    var routes = [
+      {path: "/"}, {path: "something"}, 
+    ]
+    expect(makeSignatureFromRoutes(routes, replaceLocation)).toBe("/something (REPLACE)")
   })
 })
